bf9c3e40371b823198636c120c1c9aa1b3a4181f
Harry Sadler <harrysadlermusic@gmail.com>
2020-09-21 18:46:12 +0200

    add scrape for qasa api

--- app/scripts/scrape_qasa.py
+++ app/scripts/scrape_qasa.py
@@ -4,9 +4,8 @@ sys.path.append('..')
 import json
 from service.scrape import Scrape
 
-search_term = ''
-scrape_result = Scrape.scrape_samtrygg_api(search_term)
-filepath = '/samtrygg_scrape_results.json'
+scrape_result = Scrape.scrape_qasa_api()
+filepath = '/qasa_scrape_results.json'
 with open(filepath, "w") as f:
     json_string = json.dumps(scrape_result, indent=2, sort_keys=True)
     f.write(json_string)
--- app/service/scrape.py
+++ app/service/scrape.py
@@ -3,8 +3,38 @@ import requests
 
 class Scrape():
 
+    
     @staticmethod
-    def scrape_samtrygg():
-        res = requests.get("https://www.samtrygg.se/RentalObject/SearchResult?search=")
+    def scrape_samtrygg_api(search=None):
+        req_url = "https://www.samtrygg.se/RentalObject/SearchResult"
+        if search is not None:
+            req_url += ("?search=" + search)
+        res = requests.get(req_url)
+        return res.json()
+
+    
+    @staticmethod
+    def scrape_blocket():
+        pass
+
+    
+    @staticmethod
+    def scrape_qasa_api():
+        # params:
+            # furnished: null
+            # hasPets: null
+            # maxRent: null
+            # maxRentCurrency: "SEK"
+            # maxRentalLength: null
+            # maxRoomCount: null
+            # minRentalLength: null
+            # minRoomCount: null
+            # minSquareMeters: null
+            # moveInEarliest: null
+            # moveOutEarliest: null
+            # moveOutLatest: null
+            # page: "2"
+            # perPage: 50
+        res = requests.get("https://api.qasa.se/v1/homes/search")
         return res.json()
         
