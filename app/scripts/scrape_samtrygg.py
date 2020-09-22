955bb5a99b3854454ef494f973b58f60ed420bf5
Harry Sadler <harrysadlermusic@gmail.com>
2020-09-22 17:53:31 +0200

    add formatting method for Samtrygg api results, stub other api result formatting methods for data storage

--- app/scripts/scrape_samtrygg.py
+++ app/scripts/scrape_samtrygg.py
@@ -8,10 +8,9 @@ from service.scrape import Scrape
 search_term = ''
 scrape_result = Scrape.scrape_samtrygg_api(search_term)
 # format scrape result
-listings = scrape_result['SearchResult']
-id_to_listing = {}
-for listing in listings:
-    id_to_listing[listing['ID']] = listing
+id_to_listing = Scrape.format_samtrygg_api_result_for_datastore(
+    scrape_result
+)
 # store to json file
 filepath = '/samtrygg_scrape_results.json'
 with open(filepath, "w") as f:
--- app/service/scrape.py
+++ app/service/scrape.py
@@ -4,6 +4,8 @@ import requests
 class Scrape():
 
     
+    # SAMTRYGG
+
     @staticmethod
     def scrape_samtrygg_api(search=None):
         req_url = "https://www.samtrygg.se/RentalObject/SearchResult"
@@ -12,12 +14,24 @@ class Scrape():
         res = requests.get(req_url)
         return res.json()
 
-    
+    @staticmethod
+    def format_samtrygg_api_result_for_datastore(samtrygg_api_result):
+        listings = samtrygg_api_result['SearchResult']
+        id_to_listing = {}
+        for listing in listings:
+            id_to_listing[listing['ID']] = listing
+        return id_to_listing
+
+
+    # BLOCKET
+
     @staticmethod
     def scrape_blocket():
         # NOTE: blocket and qasa use the same qasa json api
         pass
 
+
+    # QASA
     
     @staticmethod
     def scrape_qasa_api():
@@ -38,15 +52,35 @@ class Scrape():
             # perPage: 50
         res = requests.get("https://api.qasa.se/v1/homes/search")
         return res.json()
+
+    @staticmethod
+    def format_qasa_api_result_for_datastore(qasa_api_result):
+        # stub
+        pass
         
+
+    # HEMNET
 
     @staticmethod
     def scrape_hemnet_api():
         res = requests.get("https://www.hemnet.se/bostader/search/cea7d9ceb034a2be436a133ae6066e468075d8b3")
         return res.json()
 
+    @staticmethod
+    def format_hemnet_api_result_for_datastore(hemnet_api_result):
+        # stub
+        pass
+
+
+    # BOOLI
 
     @staticmethod
     def scrape_booli_api():
         res = requests.get("https://www.booli.se/1.json?sort=published&direction=desc&page=1&upcomingSale=")
         return res.json()
+
+    @staticmethod
+    def format_booli_api_result_for_datastore(booli_api_result):
+        # stub
+        pass
+
