95a06ee90726b054a66ae3bc391291b269d46dc1
Harry Sadler <harrysadlermusic@gmail.com>
2020-11-20 22:28:54 +0100

    get basic blocket scrape working, add json endpoint for blocket scrape

--- app/server.py
+++ app/server.py
@@ -1,3 +1,4 @@
+import requests
 import json
 from flask import (
     Flask,
@@ -6,7 +7,6 @@ from flask import (
     make_response,
     jsonify
 )
-
 import config.scrape_config as scrape_config
 import config.samtrygg_processing_1 as samtrygg_processing_config
 from model.samtrygg_listing import SamtryggListing
@@ -27,45 +27,6 @@ app = Flask(
 @app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({ 'output': 'hi there' })
-
-
-# get all Samtrygg results
-@app.route('/get_all_samtrygg_results', methods=['GET'])
-def get_all_samtrygg_results():
-    id_to_raw_listing = ApartmentListingDatastore.load_samtrygg_data(
-        scrape_config.SAMTRYGG_DATASTORE_FILEPATH
-    )
-    result = {
-        'result_count': len(id_to_raw_listing),
-        'result': id_to_raw_listing
-    }
-    return jsonify(result)
-
-
-# get processed Samtrygg results
-@app.route('/get_processed_samtrygg_results', methods=['GET'])
-def get_processed_samtrygg_results():
-    id_to_processed_raw_listing = ApartmentListingDatastore.load_samtrygg_data(
-        scrape_config.SAMTRYGG_PROCESSED_DATASTORE_FILEPATH
-    )
-    result = {
-        'result_count': len(id_to_processed_raw_listing),
-        'result': id_to_processed_raw_listing
-    }
-    return jsonify(result)
-
-
-# get unseen Samtrygg results
-@app.route('/get_unseen_samtrygg_results', methods=['GET'])
-def get_unseen_samtrygg_results():
-    id_to_unseen_raw_listing = ApartmentListingDatastore.load_samtrygg_data(
-        scrape_config.SAMTRYGG_PROCESSED_UNSEEN_DATASTORE_FILEPATH
-    )
-    result = {
-        'result_count': len(id_to_unseen_raw_listing),
-        'result': id_to_unseen_raw_listing
-    }
-    return jsonify(result)
 
 
 # all samtrygg listings
@@ -127,6 +88,70 @@ def get_samtrygg_cities():
     return '<h1>All Samtrygg Cities:</h1><p>{}</p>'.format(
         '<br>'.join(set(cities))
     )
+
+
+# test blocket scrape
+@app.route('/blocket/relevant/json')
+def get_relevant_blocket_results_json():
+    url = 'https://api.qasa.se/v1/homes/search'
+    headers = {
+        "Connection": "keep-alive",
+        "Accept": "application/json",
+        "X-Brand": "X-Brand",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36",
+        "Content-Type": "application/json",
+        "Origin": "https://bostad.blocket.se",
+        "Sec-Fetch-Site": "cross-site",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
+        "Referer": "https://bostad.blocket.se/",
+        "Accept-Language": "en-US,en;q=0.9,ko;q=0.8,hr;q=0.7,sv;q=0.6"
+    }
+    search_string = None
+    # search_string = "Stockholm" # testing
+    data = {
+        "minRoomCount":"3",
+        "maxRoomCount":None,
+        "maxRent":"24000",
+        "currency":None,
+        "minRentalLength":"2592000",
+        "maxRentalLength":"62208000",
+        "minSquareMeters":"50",
+        "moveInEarliest":"2020-12-01",
+        "moveOutEarliest":None,
+        "moveOutLatest":None,
+        "sharedHomeOk":False,
+        "hasPets":True,
+        "requiresWheelchairAccessible":False,
+        "furnished":"furnished_both",
+        "safeRental":False,
+        "homeType":[
+            "apartment",
+            "terrace_house",
+            "loft",
+            "duplex"
+        ],
+        "matchingArea":None,
+        "commuteLocation":{
+            "shortName": search_string,
+            "searchString": search_string,
+            "latitude":None,
+            "longitude":None,
+            "placeId":56,
+            "country":"Sweden",
+            "countryCode":"SE"
+        },
+        "areaId":56,
+        "helper":{},
+        "maxRentSek":"24000",
+        "maxRentEur":2400,
+        "maxRentCurrency":"SEK",
+        "token":"06e64aa419c6d37a1ea6fd4859fc2546",
+        "page":1,
+        "perPage":50
+    }
+    res = requests.post(url, data=json.dumps(data), headers=headers)
+    return res.json()
 
 
 # run the app if executed as main file from python interpreter
